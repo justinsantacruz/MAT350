fd49f60a8f471a9664f6a1aef1c7e5f8214b160f
justinsantacruz <97710337+justinsantacruz@users.noreply.github.com>
2023-01-20 23:50:37 -0700

    Add files via upload
    folder for hw1

---
+++ anime_3.m
@@ -0,0 +1,20 @@
+xx= linspace(-5,5,200);
+Nrep=100;
+
+
+v = VideoWriter('MAT350_sample2.m4a');
+open(v);
+
+figure;
+  
+for ind=1:Nrep
+    tt=(ind-1)*0.05;
+     plot(xx,cos(2*pi*(xx-tt))+cos(2*pi*(xx+tt)));
+    axis([-2,2,-1.5,1.5]);
+    frame = getframe(gcf);
+    writeVideo(v,frame);
+    pause(0.1);
+
+
+end
+close(v);
